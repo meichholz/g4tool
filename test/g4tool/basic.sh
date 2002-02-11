49cf1a6f96a3dc386bafd3866c246a508de6cd64
eichholz <eichholz>
2002-02-11 16:54:42 +0000

    backport of sm3600 fixes, global constants

--- test/g4tool/basic.sh
+++ test/g4tool/basic.sh
@@ -5,7 +5,7 @@ md5sum temp.pbm
 $G4TOOL -w 600 -h 600 2>&1 < ../../testfiles/rand.600x600.g4 > temp.pbm
 md5sum temp.pbm
 $G4TOOL -o pcl -b 2>&1 < ../../testfiles/test.bacon > temp.pcl
-lpr -Ppcl temp.pcl
+# lpr -Ppcl temp.pcl
 md5sum temp.pcl
 
 rm temp.*
